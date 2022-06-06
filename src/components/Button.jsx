6f382b93b8f8312bc483b0f7430aa9b054053f4e
axel alejandro morel <alejandro.morel1905@gmail.com>
2022-06-06 16:52:15 -0300

    Add button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+const Button = (props) => {
+    return (
+        <>
+            <button className="btn">
+                {props.buttonLabel}
+            </button>
+        </>
+    )
+}
+export default Button;
