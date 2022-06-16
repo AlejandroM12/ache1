d7dae24990436187500fb6e08d8d3a13e79527d1
AlejandroM12 <alejandro.morel@gmail.com>
2022-06-16 12:43:41 -0300

    Fixing navigation menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,70 +1,40 @@
-import { useRef, useState } from "react";
+import {  useState } from "react";
 import { Link } from "react-router-dom";
-
+import hamburguerIcon from "../assets/Icons/icon-menu.svg";
+import closeHamburguerIcon from "../assets/Icons/x.svg";
 const Navbar = () => {
-  const [menuToggle, setMenuToggle] = useState(false);
-  const [pivot, setPivot] = useState(1);
-  const refNav = useRef();
-  const refMenu = useRef();
-  const refBtnToggle = useRef();
-  const refIcon = useRef();
-  const handleToggle = () => {
-    if (menuToggle) {
-      setMenuToggle(false);
-      refMenu.current.style.top = "-100%";
-      refIcon.current.classList.replace("bi-x-lg", "bi-list");
-    } else {
-      setMenuToggle(true);
-      const calculate =
-        refNav.current.offsetTop + refNav.current.clientHeight - 17;
-      refMenu.current.style.top = calculate + "px";
-      refIcon.current.classList.replace("bi-list", "bi-x-lg");
-    }
-  };
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
   return (
     <>
-      <nav ref={refNav} className="menu">
-        <button ref={refBtnToggle} onClick={handleToggle} className="btnToggle">
-          <i ref={refIcon} className="bi bi-list fs-1"></i>
-        </button>
-        <ul className="ul" ref={refMenu}>
-          <li className="li">
-            <Link
-              to="/services"
-              className={pivot === 2 ? "btnSelected" : ""}
-              onClick={() => {
-                setPivot(2);
-                console.log(pivot);
-              }}
-            >
-              Services
-            </Link>
-          </li>
-          <li className="li">
-            <Link
-              to="/about"
-              className={pivot === 3 ? "btnSelected" : ""}
-              onClick={() => {
-                setPivot(3);
-                console.log(pivot);
-              }}
-            >
-              About
-            </Link>
-          </li>
-          <li className="li">
-            <Link
-              to="/contact"
-              className={pivot === 4 ? "btnSelected" : ""}
-              onClick={() => {
-                setPivot(4);
-                console.log(pivot);
-              }}
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+      <nav className="navigation">
+        { isNavExpanded === true ? (
+          <button className="hamburguer-icon" onClick ={()=>{ 
+            setIsNavExpanded(!isNavExpanded)}}><img src={hamburguerIcon} alt="hamburguer icon" /></button>
+        ):(
+          <button className="hamburguer-icon-close" onClick ={()=>{ 
+            setIsNavExpanded(!isNavExpanded)}}><img src={closeHamburguerIcon} alt="hamburguer icon close" /></button>
+        )
+      }
+        
+        <div className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
+          <ul>
+              <li>
+                <Link to="/services" className="link-a">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="link-a">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="link-a">
+                  Contact
+                </Link>
+              </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,26 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "../pages/about/About";
-import Services from "../pages/services/Services"
+import Services from "../pages/services/Services";
 import Home from "../pages/home/Home";
 import Contact from "../pages/contact/Contact";
 
-const App = ({ pivot, setPivot }) => {
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              pivot={pivot}
-              setPivot={(a) => {
-                setPivot(a);
-              }}
-            />
-          }
-        />
-        <Route path="/services" element={<Services/>} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
