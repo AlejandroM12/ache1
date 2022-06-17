28681615bec78a630c81d33060be8187e5bb5135
AlejandroM12 <alejandro.morel@gmail.com>
2022-06-17 16:23:56 -0300

    Added EmailJS

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,66 +1,161 @@
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import { useEffect } from "react";
+
 const Form = () => {
-  return (
-    <form className="form">
-    <div className="form-container">
-      <h2 className="form-title">Say Hello!</h2>
-      <div className="form-group-container">
-        <div className="form-group">
-          <input
-            type="text"
-            id="name"
-            className="form-input"
-            placeholder=" "
-          />
-          <label className="form-label">
-            Name
-          </label>
-          <span className="form-line"></span>
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            id="company"
-            className="form-input"
-            placeholder=" "
-          />
-          <label className="form-label">
-            Company
-          </label>
-          <span className="form-line"></span>
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            id="email"
-            className="form-input"
-            placeholder=" "
-          />
-          <label className="form-label">
-            Email
-          </label>
-          <span className="form-line"></span>
-        </div>
-      </div>
-      <div className="form-group-select">
-        <label className="form-label-select">I'm interested in</label>
-        <select name="" id="" className="form-select">
-          <option value="" className="form-label-option">Bussines Branding</option>
-          <option value="" className="form-label-option">Bussines asdas</option>
-        </select>
-      </div>
-      <div className="form-group-select">
-        <label className="form-label-select">How did you hear about us?</label>
-        <select name="" id="" className="form-select">
-          <option value="" className="form-label-option">From Google</option>
-        </select>
-      </div>
-      <div className="form-group-textarea">
-        <label className="form-label-textarea">Brief description of your project ...</label>
-        <textarea name="" id="" cols="30" rows="7" ></textarea>
-      </div>
-      <button className="form-submit">SEND!</button>
+  const [status, setStatus] = useState("");
+  const [values, setValues] = useState({
+    name: "",
+    company: "",
+    email: "",
+    interestedIn: "",
+    aboutUs: "",
+    message: "",
+  });
+  console.log(values);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .send("service_u1tfsgl", "template_9es16h5", values, "cuWvUKETrKkWLF3Db")
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response);
+          setValues({
+            name: "",
+            company: "",
+            email: "",
+            interestedIn: "",
+            aboutUs: "",
+            message: "",
+          });
+          setStatus("SUCCESS");
+        },
+        (error) => {
+          console.log("FAILED!", error);
+        }
+      );
+  };
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  useEffect(() => {
+    if (status === "SUCCESS") {
+      setTimeout(() => {
+        setStatus("");
+      }, 3000);
+    }
+  }, [status]);
+  const renderAlert = () => (
+    <div className="submit-alert">
+      <p>Your message submitted successfully</p>
     </div>
-  </form>
+  );
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      {status && renderAlert()}
+      <div className="form-container">
+        <h2 className="form-title">Say Hello!</h2>
+        <div className="form-group-container">
+          <div className="form-group">
+            <input
+              onChange={handleChange}
+              type="text"
+              name="name"
+              value={values.name}
+              className="form-input"
+              placeholder=" "
+            />
+            <label className="form-label" htmlFor="name">
+              Name
+            </label>
+            <span className="form-line"></span>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange}
+              type="text"
+              name="company"
+              value={values.company}
+              className="form-input"
+              placeholder=" "
+            />
+            <label className="form-label" htmlFor="company">
+              Company
+            </label>
+            <span className="form-line"></span>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange}
+              type="text"
+              name="email"
+              value={values.email}
+              className="form-input"
+              placeholder=" "
+            />
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <span className="form-line"></span>
+          </div>
+        </div>
+        <div className="form-group-select">
+          <label className="form-label-select">I'm interested in</label>
+          <select
+            onChange={handleChange}
+            value={values.interestedIn}
+            name="interestedIn"
+            className="form-select"
+          >
+            <option value="interestedIn" className="form-label-option">
+              Bussines Branding
+            </option>
+            <option value="prueba1" className="form-label-option">
+              Prueba 1
+            </option>
+            <option value="prueba2" className="form-label-option">
+              Prueba 2
+            </option>
+          </select>
+        </div>
+        <div className="form-group-select">
+          <label className="form-label-select">
+            How did you hear about us?
+          </label>
+          <select
+            onChange={handleChange}
+            value={values.aboutUs}
+            name="aboutUs"
+            className="form-select"
+          >
+            <option value="aboutUs" className="form-label-option">
+              From Google
+            </option>
+            <option value="prueba1" className="form-label-option">
+              Prueba 1
+            </option>
+            <option value="prueba2" className="form-label-option">
+              Prueba 2
+            </option>
+          </select>
+        </div>
+        <div className="form-group-textarea">
+          <label className="form-label-textarea" htmlFor="message">
+            Brief description of your project ...
+          </label>
+          <textarea
+            onChange={handleChange}
+            name="message"
+            value={values.message}
+            cols="30"
+            rows="7"
+          ></textarea>
+        </div>
+        <button className="form-submit" type="submit">
+          SEND!
+        </button>
+      </div>
+    </form>
   );
 };
 export default Form;
