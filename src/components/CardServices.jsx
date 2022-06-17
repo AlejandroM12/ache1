f58dd8f957c7b253dd19ddddea073b2a259b03d7
AlejandroM12 <alejandro.morel@gmail.com>
2022-06-17 11:47:13 -0300

    Fixing services section

--- src/components/CardServices.jsx
+++ src/components/CardServices.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
+import { useModal } from "../hooks/useModal";
 import ContainerLeftSection from "./ContainerLeftSection";
 import ContainerRightSection from "./ContainerRightSection";
+import UseLottieAnimation from "../components/useLottieAnimation";
 import App from "../assets/Services/apps-mobile.svg";
 import Button from "./Button";
+import animationUX from "../assets/lotties/ux.json";
+import ModalApp from "./ModalsComponents/ModalApp";
+import ModalUx from "./ModalsComponents/ModalUx";
+import webAppsAnimation from "../assets/lotties/web-apps.json";
+import animationBrandingBlack from "../assets/lotties/branding-black.json";
+import socialMediaAnimation from "../assets/lotties/social-media.json";
+import marketingAnimation from "../assets/lotties/marketing.json";
 
 const CardServices = () => {
   const [linkActive, setLinkActive] = useState(1);
+  const [isOpenModalApp, openModalApp, closeModalApp] = useModal(false);
+  const [isOpenModalUX, openModalUx, closeModalUx] = useModal(false);
   return (
     <>
       <ContainerLeftSection>
@@ -112,52 +123,79 @@ const CardServices = () => {
       <ContainerRightSection>
         {linkActive === 1 && (
           <div className="content-service">
-            <div className="content-card-right" data-aos="fade-up">
+            <div
+              className="content-card-right"
+              data-aos="fade-right"
+              data-aos-easing="ease-in-out"
+            >
               <h3 className="card-title-right">Apps mobile</h3>
-              <img src={App} alt="" className="card-img-right" />
               <p className="card-description-right">
                 It makes your app unique. From usability analysis to testing to
                 launch your MVP.
               </p>
-
-              <Button buttonLabel="Our tecnologies" />
+              <Button buttonLabel="Our tecnologies" onClick={openModalApp} />
             </div>
           </div>
         )}
         {linkActive === 2 && (
           <div className="content-service">
-            <div className="content-card-right" data-aos="fade-up">
+            <div
+              className="content-card-right"
+              data-aos="fade-right"
+              data-aos-easing="ease-in-out"
+            >
               <h3 className="card-title-right">Ux/Ui</h3>
-              <img src={App} alt="" className="card-img-right" />
               <p className="card-description-right">
                 We carry out a survey of the market and the competition. We plan
                 and design the correct flow for your website to be successful.
               </p>
 
-              <Button buttonLabel="How we work?" />
+              <Button buttonLabel="How we work?" onClick={openModalUx} />
+              <div className="content-service-lottie-ux">
+                <UseLottieAnimation
+                  animationData={animationUX}
+                  autoplay={true}
+                  loop={true}
+                />
+              </div>
             </div>
           </div>
         )}
 
         {linkActive === 3 && (
           <div className="content-service">
-            <div className="content-card-right" data-aos="fade-up">
+            <div
+              className="content-card-right"
+              data-aos="fade-right"
+              data-aos-easing="ease-in-out"
+            >
               <h3 className="card-title-right">Web apps</h3>
-              <img src={App} alt="" className="card-img-right" />
               <p className="card-description-right">
                 You need to organize your business, nothing better than having
                 your own online system that your entire team can access.
               </p>
 
               <Button buttonLabel="Biggest systems" />
+              <div className="content-service-lottie-web-apps">
+                <UseLottieAnimation
+                  animationData={webAppsAnimation}
+                  autoplay={true}
+                  loop={true}
+                  width={200}
+                  height={200}
+                />
+              </div>
             </div>
           </div>
         )}
         {linkActive === 4 && (
           <div className="content-service">
-            <div className="content-card-right" data-aos="fade-up">
+            <div
+              className="content-card-right"
+              data-aos="fade-right"
+              data-aos-easing="ease-in-out"
+            >
               <h3 className="card-title-right">Best Websites</h3>
-              <img src={App} alt="" className="card-img-right" />
               <p className="card-description-right">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea non
                 sit, laudantium modi corporis nemo!
@@ -169,57 +207,82 @@ const CardServices = () => {
         )}
         {linkActive === 5 && (
           <div className="content-service">
-            <div className="content-card-right" data-aos="fade-up">
+            <div
+              className="content-card-right"
+              data-aos="fade-right"
+              data-aos-easing="ease-in-out"
+            >
               <h3 className="card-title-right">Branding</h3>
-              <img src={App} alt="" className="card-img-right" />
               <p className="card-description-right">
                 Design your brand identity from scratch. Create a unique
                 language that identifies you in the market.
               </p>
-
               <Button buttonLabel="I want start" />
+              <div className="content-service-lottie-branding">
+                <UseLottieAnimation
+                  animationData={animationBrandingBlack}
+                  autoplay={true}
+                  loop={true}
+                />
+              </div>
             </div>
           </div>
         )}
         {linkActive === 6 && (
           <div className="content-service">
-            <div className="content-card-right" data-aos="fade-up">
+            <div
+              className="content-card-right"
+              data-aos="fade-right"
+              data-aos-easing="ease-in-out"
+            >
               <h3 className="card-title-right">Social Media</h3>
-              <img src={App} alt="" className="card-img-right" />
               <p className="card-description-right">
                 We perform a competitive analysis to create your brand strategy
                 on social networks. Guidelines for communication, promotion and
                 user monitoring.
               </p>
-
               <Button buttonLabel="Your brand UP!" />
+              <div className="content-service-lottie-social-media">
+                <UseLottieAnimation
+                  animationData={socialMediaAnimation}
+                  autoplay={true}
+                  loop={true}
+                />
+              </div>
             </div>
           </div>
         )}
         {linkActive === 7 && (
           <div className="content-service">
-            <div className="content-card-right" data-aos="fade-up">
+            <div className="content-card-right" data-aos="fade-right">
+              <div className="content-service-lottie-marketing">
+                <UseLottieAnimation
+                  animationData={marketingAnimation}
+                  autoplay={true}
+                  loop={true}
+                />
+              </div>
               <h3 className="card-title-right">Marketing strategy</h3>
-              <img src={App} alt="" className="card-img-right" />
               <p className="card-description-right">
                 Use our services and build a solid digital presence across
                 digital marketing, creative web design, quality content, SEO,
                 and media campaigns.
               </p>
-
               <Button buttonLabel="Grow up now" />
             </div>
           </div>
         )}
         {linkActive === 8 && (
           <div className="content-service">
-            <div className="content-card-right" data-aos="fade-up">
-              <h3 className="card-title-right">Marketing strategy</h3>
-              <img src={App} alt="" className="card-img-right" />
+            <div
+              className="content-card-right"
+              data-aos="fade-right"
+              data-aos-easing="ease-in-out"
+            >
+              <h3 className="card-title-right">Editorial</h3>
               <p className="card-description-right">
-                Use our services and build a solid digital presence across
-                digital marketing, creative web design, quality content, SEO,
-                and media campaigns.
+                Make your project a reality and turn it into a successful book.
+                Hand in hand with the best editorial designers on the market.
               </p>
 
               <Button buttonLabel="Grow up now" />
@@ -227,6 +290,12 @@ const CardServices = () => {
           </div>
         )}
       </ContainerRightSection>
+      {isOpenModalApp && (
+        <ModalApp isOpen={isOpenModalApp} closeModal={closeModalApp} />
+      )}
+      {isOpenModalUX && (
+        <ModalUx isOpen={isOpenModalUX} closeModal={closeModalUx} />
+      )}
     </>
   );
 };
