9c4953b02f9d7167d1445b85e47ba11268cce53f
AlejandroM12 <alejandro.morel@gmail.com>
2022-06-22 14:51:02 -0300

    Added mobile contact

---
+++ src/components/FormMobile.jsx
@@ -0,0 +1,177 @@
+import { useState, useEffect } from "react";
+import Lottie from "lottie-react";
+import emailjs from "@emailjs/browser";
+import animationCoffe from "../assets/lotties/coffee-contact.json";
+
+const FormMobile = () => {
+  const [status, setStatus] = useState("");
+  const [values, setValues] = useState({
+    name: "",
+    company: "",
+    email: "",
+    interestedIn: "",
+    aboutUs: "",
+    message: "",
+  });
+  console.log(values);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .send("service_u1tfsgl", "template_9es16h5", values, "cuWvUKETrKkWLF3Db")
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response);
+          setValues({
+            name: "",
+            company: "",
+            email: "",
+            interestedIn: "",
+            aboutUs: "",
+            message: "",
+          });
+          setStatus("SUCCESS");
+        },
+        (error) => {
+          console.log("FAILED!", error);
+        }
+      );
+  };
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  useEffect(() => {
+    if (status === "SUCCESS") {
+      setTimeout(() => {
+        setStatus("");
+      }, 3000);
+    }
+  }, [status]);
+  //   const renderAlert = () => (
+  //     <div className="submit-alert">
+  //       <p>Your message submitted successfully</p>
+  //     </div>
+  //   );
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      {status}
+      <div className="form-container">
+        <div className="contact-content-right">
+          <div className="contact-lottie-right">
+            <Lottie animationData={animationCoffe} />
+          </div>
+          <h3 className="contact-title-right">
+            Let´s work <br /> together
+          </h3>
+
+          <p className="contact-description-right">
+            Tell us more about your project.
+          </p>
+          <p className="contact-description-right">
+            <span>Let's drink a coffee.</span>
+          </p>
+        </div>
+        <h2 className="form-title">Say Hello!</h2>
+        <div className="form-group-container">
+          <div className="form-group">
+            <input
+              onChange={handleChange}
+              type="text"
+              name="name"
+              value={values.name}
+              className="form-input"
+              placeholder=" "
+            />
+            <label className="form-label" htmlFor="name">
+              Name
+            </label>
+            <span className="form-line"></span>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange}
+              type="text"
+              name="company"
+              value={values.company}
+              className="form-input"
+              placeholder=" "
+            />
+            <label className="form-label" htmlFor="company">
+              Company
+            </label>
+            <span className="form-line"></span>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange}
+              type="text"
+              name="email"
+              value={values.email}
+              className="form-input"
+              placeholder=" "
+            />
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <span className="form-line"></span>
+          </div>
+        </div>
+        <div className="form-group-select">
+          <label className="form-label-select">I'm interested in</label>
+          <select
+            onChange={handleChange}
+            value={values.interestedIn}
+            name="interestedIn"
+            className="form-select"
+          >
+            <option value="interestedIn" className="form-label-option">
+              Bussines Branding
+            </option>
+            <option value="prueba1" className="form-label-option">
+              Prueba 1
+            </option>
+            <option value="prueba2" className="form-label-option">
+              Prueba 2
+            </option>
+          </select>
+        </div>
+        <div className="form-group-select">
+          <label className="form-label-select">
+            How did you hear about us?
+          </label>
+          <select
+            onChange={handleChange}
+            value={values.aboutUs}
+            name="aboutUs"
+            className="form-select"
+          >
+            <option value="aboutUs" className="form-label-option">
+              From Google
+            </option>
+            <option value="prueba1" className="form-label-option">
+              Prueba 1
+            </option>
+            <option value="prueba2" className="form-label-option">
+              Prueba 2
+            </option>
+          </select>
+        </div>
+        <div className="form-group-textarea">
+          <label className="form-label-textarea" htmlFor="message">
+            Brief description of your project ...
+          </label>
+          <textarea
+            onChange={handleChange}
+            name="message"
+            value={values.message}
+            cols="30"
+            rows="7"
+          ></textarea>
+        </div>
+        <button className="form-submit" type="submit">
+          SEND!
+        </button>
+      </div>
+    </form>
+  );
+};
+export default FormMobile;
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useViewport } from "../../context/viewportContext";
 import Lottie from "lottie-react";
 import ContainerSection from "../../layout/ContainerSection";
 import ContainerHeaderSection from "../../components/ContainerHeaderSection";
@@ -10,12 +11,17 @@ import Form from "../../components/Form";
 import animationCoffe from "../../assets/lotties/coffee-contact.json";
 import logoWhite from "../../assets/Icons/logo-white.svg";
 import LogoHome from "../../components/LogoHome";
+import FormMobile from "../../components/FormMobile";
+import linkedinIcon from "../../assets/Icons/linkedin-blue.svg";
+import instagramIcon from "../../assets/Icons/instagram-blue.svg";
 
 const Contact = () => {
-  return (
+  const { width } = useViewport();
+  const breakpoint = 421;
+  return width > breakpoint ? (
     <ContainerSection>
       <ContainerHeaderSection>
-      <LogoHome logo={logoWhite}/>
+        <LogoHome logo={logoWhite} />
         <Navbar />
       </ContainerHeaderSection>
       <Link to="/" className="icon-contact">
@@ -26,15 +32,43 @@ const Contact = () => {
       </ContainerLeftSection>
       <ContainerRightSection>
         <div className="contact-content-right">
-            <h3 className="contact-title-right">Let´s work <br /> together</h3>
-            <div className="contact-lottie-right">
-                <Lottie animationData={animationCoffe}/>
-            </div>
-            <p className="contact-description-right">Tell us more about your project. 
-            </p>
-            <p className="contact-description-right"><span>Let's drink a coffee.</span></p>
+          <h3 className="contact-title-right">
+            Let´s work <br /> together
+          </h3>
+          <div className="contact-lottie-right">
+            <Lottie animationData={animationCoffe} />
+          </div>
+          <p className="contact-description-right">
+            Tell us more about your project.
+          </p>
+          <p className="contact-description-right">
+            <span>Let's drink a coffee.</span>
+          </p>
         </div>
       </ContainerRightSection>
+    </ContainerSection>
+  ) : (
+    <ContainerSection>
+      <ContainerHeaderSection>
+        <LogoHome logo={logoWhite} />
+        <Navbar />
+      </ContainerHeaderSection>
+      <Link to="/" className="icon-contact">
+        <Icon size={45} color="#6C63FF" fill="#6C63FF" />
+      </Link>
+      <FormMobile />
+      <div className="social-media-container-mobile-contact">
+        <li>
+          <Link to="/" className="social-media-mobile-contact">
+            <img src={linkedinIcon} alt="linkedin icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="social-media-mobile-contact">
+            <img src={instagramIcon} alt="instagram icon" />
+          </Link>
+        </li>
+      </div>
     </ContainerSection>
   );
 };
