121c4cbf4286c4816cc2cb0876fd7ccb294d0dd6
AlejandroM12 <alejandro.morel@gmail.com>
2022-06-21 23:05:36 -0300

    Added menu navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,83 @@
-import {  useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import hamburguerIcon from "../assets/Icons/icon-menu.svg";
 import closeHamburguerIcon from "../assets/Icons/x.svg";
+import linkedinIcon from "../assets/Icons/linkedin.svg";
+import instagramIcon from "../assets/Icons/instagram.svg";
+
 const Navbar = () => {
-  const [isNavExpanded, setIsNavExpanded] = useState(true)
+  const [isNavExpanded, setIsNavExpanded] = useState(true);
   return (
     <>
       <nav className="navigation">
-        { isNavExpanded === true ? (
-          <button className="hamburguer-icon" onClick ={()=>{ 
-            setIsNavExpanded(!isNavExpanded)}}><img src={hamburguerIcon} alt="hamburguer icon" /></button>
-        ):(
-          <button className="hamburguer-icon-close" onClick ={()=>{ 
-            setIsNavExpanded(!isNavExpanded)}}><img src={closeHamburguerIcon} alt="hamburguer icon close" /></button>
-        )
-      }
-        
-        <div className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
+        {isNavExpanded === true ? (
+          <button
+            className="hamburguer-icon"
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}
+          >
+            <img src={hamburguerIcon} alt="hamburguer icon" />
+          </button>
+        ) : (
+          <button
+            className="hamburguer-icon-close"
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}
+          >
+            <img src={closeHamburguerIcon} alt="hamburguer icon close" />
+          </button>
+        )}
+
+        <div
+          className={
+            isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+          }
+        >
           <ul>
+            <li>
+              <NavLink
+                className={({ isActive }) => {
+                  return isActive ? "active" : "link-a";
+                }}
+                to="/services"
+              >
+                Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => {
+                  return isActive ? "active" : "link-a";
+                }}
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => {
+                  return isActive ? "active" : "link-a";
+                }}
+                to="/contact"
+              >
+                Contact
+              </NavLink>
+            </li>
+            <div className="social-media-container">
               <li>
-                <NavLink className={({ isActive }) => {
-                  return isActive ? 'active' : "link-a"
-                }} to='/services' >
-                  Services
-                </NavLink>
+                <Link to="/" className="social-media">
+                  <img src={linkedinIcon} alt="linkedin icon" />
+                </Link>
               </li>
               <li>
-              <NavLink className={({ isActive }) => {
-                  return isActive ? 'active' : "link-a"
-                }} to='/about' >
-                  About
-                </NavLink>
+                <Link to="/" className="social-media">
+                  <img src={instagramIcon} alt="instagram icon" />
+                </Link>
               </li>
-              <li>
-              <NavLink className={({ isActive }) => {
-                  return isActive ? 'active' : "link-a"
-                }} to='/contact' >
-                  Contact
-                </NavLink>
-              </li>
+            </div>
           </ul>
         </div>
       </nav>
