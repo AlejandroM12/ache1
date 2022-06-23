0f67caf4c59da8d3e0fec3fd0c55c4bf9a0c57c2
AlejandroM12 <alejandro.morel@gmail.com>
2022-06-23 20:29:07 -0300

    Added mobile modal App

--- src/components/ModalsComponents/ModalApp.jsx
+++ src/components/ModalsComponents/ModalApp.jsx
@@ -4,28 +4,44 @@ import netLogo from "../../assets/Modal/net.svg";
 import ContainerLeftModal from "../ContainerLeftModal";
 import ContainerRightModal from "../ContainerRightModal";
 import ContainerModal from "../../layout/ContainerModal";
-const ModalApp = ({isOpen, closeModal}) => {
+import UseLottieAnimation from "../../components/useLottieAnimation";
+import animationAppMobile from "../../assets/lotties/apps-mobile.json";
+
+const ModalApp = ({ isOpen, closeModal }) => {
   return (
     <>
-    <ContainerModal >
-      <div className={`modal${isOpen && "is-open"}`} ></div>
-      <ContainerLeftModal>
-        <div className="lottie-modal-app"></div>
-        <h3 className="modal-app-title-left">Apps mobile</h3>
-        <p className="modal-app-description-left">
-          We are ready to create your app <span>in any language you want.</span>{" "}
-          Respecting the latest trends in technologies.
-        </p>
-      </ContainerLeftModal>
-      <ContainerRightModal>
-        <h3 className="modal-title-app-right">We believe in this languages</h3>
-        <div className="modal-app-img">
-          <img src={nodeLogo} alt="" />
-          <img src={reactLogo} alt="" />
-          <img src={netLogo} alt="" />
-        </div>
-        <button className="close-modal" onClick={closeModal}>X</button>
-      </ContainerRightModal>
+      <ContainerModal>
+        <div className={`modal${isOpen && "is-open"}`}></div>
+        <ContainerLeftModal>
+          <div className="container-description-modal-app-mobile">
+            <div className="lottie-modal-app-mobile">
+              <UseLottieAnimation
+                animationData={animationAppMobile}
+                autoplay={true}
+                loop={true}
+              />
+            </div>
+            <h3 className="modal-app-title-left">Apps mobile</h3>
+            <p className="modal-app-description-left">
+              We are ready to create your app{" "}
+              <span>in any language you want.</span> Respecting the latest
+              trends in technologies.
+            </p>
+          </div>
+        </ContainerLeftModal>
+        <ContainerRightModal>
+          <h3 className="modal-title-app-right">
+            We believe in this languages
+          </h3>
+          <div className="container-modal-app-img">
+            <img src={nodeLogo} className="app-modal-img" alt="nodejs" />
+            <img src={reactLogo} className="app-modal-img" alt="reactjs" />
+            <img src={netLogo} className="app-modal-img" alt=".net" />
+          </div>
+          <button className="close-modal" onClick={closeModal}>
+            X
+          </button>
+        </ContainerRightModal>
       </ContainerModal>
     </>
   );
